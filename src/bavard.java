f107759bff012015a08e76f41b6e74d683823aff
Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>
2021-05-17 03:07:15 +0200

    Create bavard.java

---
+++ src/bavard.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bavard implements PapotageListener {
+	
+		private String sujet;
+		
+		public Bavard (String sujet) {
+			this.sujet = sujet;
+		}
+
+		public void setsujet(String sujet) {
+			this.sujet = sujet;
+		}
+
+	
+		
+		public String getsujet() {
+			return this.sujet;
+		}
+		
+		@Override
+		public void ecouteMessage(PapotageEvent even) {
+			System.out.println(this.sujet + "A recu: " + sujet + " " + event);
+		}
+	}

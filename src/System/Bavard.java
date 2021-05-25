d226cd9c1da0756e71c30c81c23816b32a35d2c9
Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>
2021-05-25 22:38:02 +0200

    Update Bavard.java

--- src/System/Bavard.java
+++ src/System/Bavard.java
@@ -1,26 +1,100 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Bavard implements PapotageListener {
-	
-		private String sujet;
-		
-		public Bavard (String sujet) {
-			this.sujet = sujet;
-		}
-
-		public void setsujet(String sujet) {
-			this.sujet = sujet;
-		}
-
+		public String login;
+		public String password;
+		public Messagerie mess;
+		public String ok;
 	
 		
-		public String getsujet() {
-			return this.sujet;
+		
+		// Constructors
+		
+		Bavard(String login,String password){
+			this.login=login;
+			this.password=password;
+			this.ok=String.valueOf(mess);
+			
 		}
 		
+		
+		/**
+		 * Constructor with generated login and password
+		 */
+		
+		Bavard(){
+			Random r = new Random();
+			char c = (char)(r.nextInt(26) + 'a');
+			String random = "";
+			int n = 3;
+			for(int i=0; i < n; i++) {
+				  random = random + c;
+			}
+			
+			this.login=random;
+			this.password=random;
+			this.ok=String.valueOf(mess);
+			
+		}
+		
+		
+		public PapotageEvent generEvent(String sujet,String corps) {
+			PapotageEvent P=new PapotageEvent(this,sujet,corps);
+			this.login=login;
+			return P;
+			
+		}
+		
+	
+
 		@Override
 		public void ecouteMessage(PapotageEvent even) {
-			System.out.println(this.sujet + "A recu: " + sujet + " " + event);
+			
+			this.mess.ecouteMessage(even);
 		}
-	}
+		
+		
+		@Override
+		public String toString() {
+			return "["+ login + "|" + password + "]";
+		}
+		
+		public String toString2() {
+			return "Bavard "+login+" est crée son mp: "+password ; 
+		}
+		
+		public Messagerie getMess() {
+			return mess;
+		}
+		
+		// getters and setters
+		public void setMess(Messagerie mess) {
+			this.mess=mess;
+		}
+
+		public String getLogin() {
+			return login;
+		}
+
+
+		public void setLogin(String login) {
+			this.login = login;
+		}
+
+
+		public String getPassword() {
+			return password;
+		}
+
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		
+}
+		
+
+
+
